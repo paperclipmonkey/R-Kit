0ebfb82d11d8f700d89b4bb0551d85b99af0b3e7
Michael Waterworth <mike@3equals.co.uk>
2015-08-18 16:16:07 +0100

    Add more files to support listing upcoming tasks

---
+++ app/src/main/java/com/example/michaelwaterworth/r_kit/MainFragment.java
@@ -0,0 +1,100 @@
+package com.example.michaelwaterworth.r_kit;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+/**
+ * Created by michaelwaterworth on 16/08/15.
+ */
+public class MainFragment extends Fragment{
+    ListView listView;
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static MainFragment newInstance(int sectionNumber) {
+        MainFragment fragment = new MainFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public MainFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //View base = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        // Create a progress bar to display while the list loads
+        ProgressBar progressBar = new ProgressBar(getActivity());
+        progressBar.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT, Gravity.CENTER));
+        progressBar.setIndeterminate(true);
+
+        listView = (ListView) getActivity().findViewById(R.id.taskslist);
+        listView.setEmptyView(progressBar);
+
+        // Must add the progress bar to the root of the layout
+        ViewGroup root = (ViewGroup) getActivity().findViewById(android.R.id.content);
+        root.addView(progressBar);
+
+
+        //Check in Db - see if there are any upcoming Task
+        Calendar cal = Calendar.getInstance();
+
+        long sTime = cal.getTimeInMillis() / 1000;
+        TaskAdapter adapter = new TaskAdapter(Task.find(Task.class, "date > ?", "" + sTime));
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                // ListView Clicked item index
+                int itemPosition = i;
+
+                // ListView Clicked item value
+                String itemValue = (String) listView.getItemAtPosition(i);
+
+                // Show Alert
+                Toast.makeText(getActivity(),
+                        "Position :" + itemPosition + "  ListItem : " + itemValue, Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+
+        return root;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((MainActivity) activity).onSectionAttached(
+                getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+
+}
---
+++ app/src/main/java/com/example/michaelwaterworth/r_kit/TaskAdapter.java
@@ -0,0 +1,64 @@
+package com.example.michaelwaterworth.r_kit;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by michaelwaterworth on 16/08/15.
+ */
+public class TaskAdapter extends BaseAdapter {
+    private List<Task> mTasks;
+
+    public TaskAdapter(List<Task> tasks) {
+        mTasks = tasks;
+    }
+
+    @Override
+    public int getCount() {
+        return mTasks.size();
+    }
+
+    @Override
+    public Task getItem(int i) {
+        return mTasks.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+        //return mTasks.get(i).getId();
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View V = convertView;
+
+        if(V == null) {
+            LayoutInflater vi = (LayoutInflater)parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            V = vi.inflate(R.layout.task_row, null);
+        }
+
+        Task ti = mTasks.get(position);
+        ImageView icon = (ImageView)V.findViewById(R.id.task_image);
+        TextView title = (TextView)V.findViewById(R.id.task_title);
+        TextView artist = (TextView)V.findViewById(R.id.task_description);
+
+//            if(ti.isSelected()) {
+//                icon.setImageResource(R.drawable.item_selected);
+//            }
+//            else {
+//                icon.setImageResource(R.drawable.item_unselected);
+//            }
+
+        title.setText(ti.getNotifTitle());
+        artist.setText("by " + ti.getNotifDesc());
+
+        return V;
+    }
+}

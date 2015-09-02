fcc996d818741da39d07f832ecd0f74e305eeb3f
Michael Waterworth <mike@3equals.co.uk>
2015-09-02 12:30:38 +0100

    Add Upload Service

--- app/src/main/java/com/example/michaelwaterworth/r_kit/BaseApplication.java
+++ app/src/main/java/com/example/michaelwaterworth/r_kit/BaseApplication.java
@@ -1,10 +1,17 @@
 package com.example.michaelwaterworth.r_kit;
 
+import com.alexbbb.uploadservice.UploadService;
+
 /**
  * Created by michaelwaterworth on 30/07/15. Copyright Michael Waterworth
 
  */
+
 public class BaseApplication extends com.orm.SugarApp{
 
-
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        UploadService.NAMESPACE = "com.example.michaelwaterworth.r_kit";
+    }
 }
